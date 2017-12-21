4a2577391740b61b49dff76e8a2b2d41d2ce471a
Evan McCoy <mccoythedev@gmail.com>
2017-12-21 21:25:35 +0000

    Fixed database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('ocrfid.db')
 
 c = conn.cursor()
 c.execute('''CREATE TABLE settings (attribute text, value text)''')
-c.execute('''INSERT INTO settings (attribute, value) VALUES(currentEvent, build)''')
-c.execute('''INSERT INTO settings (attribute, value) VALUES(event, build)''')
+c.execute('''INSERT INTO settings (attribute, value) VALUES("currentEvent", "Build 2018")''')
+c.execute('''INSERT INTO settings (attribute, value) VALUES("event", "Build 2018")''')
 c.execute('''CREATE TABLE members (uuid text, name text, register_time bigint)''')
 c.execute('''CREATE TABLE timesheet (uuid text, event text, in_time bigint, out_time bigint)''')
 
